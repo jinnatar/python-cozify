91b60d6e9104d5d0475ee7e3f65aa182344c0b0d
Artanicus <artanicus@nocturnal.fi>
2017-02-12 18:08:42 +0200

    Auth work, not fully complete yet at this stage

--- cozify/config.py
+++ cozify/config.py
@@ -2,3 +2,16 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read('main.cfg')
+
+# prime ephemeral storage
+try:
+    file = open(config['Cloud']['ephemeral'], 'r')
+except IOError:
+    file = open(config['Cloud']['ephemeral'], 'w')
+
+ephemeral = configparser.ConfigParser()
+ephemeral.read(config['Cloud']['ephemeral'])
+
+def ephemeralWrite():
+    with open(config['Cloud']['ephemeral'], 'w') as configfile:
+        ephemeral.write(configfile)
