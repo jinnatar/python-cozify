5bc177b0e6db05b9e9f25e0e6e53dd5a1ac02914
Artanicus <artanicus@nocturnal.fi>
2017-02-21 19:27:17 +0200

    Improve setup.py to actually work

--- setup.py
+++ setup.py
@@ -5,9 +5,17 @@ with open('README.md') as file:
         long_description = file.read()
 
 setup(name='cozify',
-        version='0.1',
-        author='artanicus',
-        author_email='python-cozify@nocturnal.fi',
-        url='https://github.com/Artanicus/python-cozify',
-        long_description=long_description,
+        version = '0.1',
+        author = 'artanicus',
+        author_email = 'python-cozify@nocturnal.fi',
+        url = 'https://github.com/Artanicus/python-cozify',
+        description = 'Unofficial Python bindings and helpers for the unpublished Cozify API.',
+        long_description = long_description,
+        license = 'MIT',
+        packages = ['cozify'],
+        classifiers = [
+            "Development Status :: 3 - Alpha",
+            "Topic :: Utilities",
+            "License :: OSI Approved :: MIT License",
+            ]
         )
