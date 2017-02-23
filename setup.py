4b44c4ab09fa0246192abddf674dcaad908a12df
Artanicus <artanicus@nocturnal.fi>
2017-02-23 22:06:54 +0200

    Bump version to 0.2.1, anything beyond this point is between versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as file:
         long_description = file.read()
 
 setup(name='cozify',
-        version = '0.2',
+        version = '0.2.1',
         author = 'artanicus',
         author_email = 'python-cozify@nocturnal.fi',
         url = 'https://github.com/Artanicus/python-cozify',
