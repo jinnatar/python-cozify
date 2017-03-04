4836cb93b03eae38c0e1eebeee831f9b4fc012eb
Artanicus <artanicus@nocturnal.fi>
2017-03-04 17:57:54 +0200

    Support for changing ephemeral state storage mid-run. Mostly useful for debugging and testing without hosing your main state

--- cozify/config.py
+++ cozify/config.py
@@ -1,23 +1,40 @@
 import configparser
 import os
 
+ephemeralFile = "%s/.config/python-cozify.cfg" % os.path.expanduser('~')
+ephemeral = None
+
+
 def ephemeralWrite():
     with open(ephemeralFile, 'w') as configfile:
         ephemeral.write(configfile)
 
-# prime ephemeral storage
-ephemeralFile = "%s/.config/python-cozify.cfg" % os.path.expanduser('~')
-try:
-    file = open(ephemeralFile, 'r')
-except IOError:
-    file = open(ephemeralFile, 'w+')
-    os.chmod(ephemeralFile, 0o600) # set to user readwrite only to protect tokens
+# allow setting the ephemeral storage location.
+# Useful especially for testing without affecting your normal state
+def setStatePath(filepath):
+    global ephemeralFile
+    ephemeralFile = filepath
+    _initState()
 
-ephemeral = configparser.ConfigParser()
-ephemeral.read(ephemeralFile)
 
-# make sure config is in roughly a valid state
-for key in [ 'Cloud', 'Hubs' ]:
-    if key not in ephemeral:
-        ephemeral[key] = {}
-ephemeralWrite()
+
+def _initState():
+    global ephemeral
+
+    # prime ephemeral storage
+    try:
+        file = open(ephemeralFile, 'r')
+    except IOError:
+        file = open(ephemeralFile, 'w+')
+        os.chmod(ephemeralFile, 0o600) # set to user readwrite only to protect tokens
+
+    ephemeral = configparser.ConfigParser()
+    ephemeral.read(ephemeralFile)
+
+    # make sure config is in roughly a valid state
+    for key in [ 'Cloud', 'Hubs' ]:
+        if key not in ephemeral:
+            ephemeral[key] = {}
+    ephemeralWrite()
+
+_initState()
