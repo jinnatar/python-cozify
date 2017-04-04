fc3f72294e41a966cb8cc728de7afa271d2994ee
Artanicus <artanicus@nocturnal.fi>
2017-04-04 16:43:42 +0300

    Change to XDG compatible config placement. Should now work better on systems where XDG has been customized or hasn't been deployed at all yet

--- cozify/config.py
+++ cozify/config.py
@@ -1,7 +1,23 @@
 import configparser
 import os
 
-stateFile = "%s/.config/python-cozify.cfg" % os.path.expanduser('~')
+# per the XDG basedir-spec we adhere to $XDG_CONFIG_HOME if it's set, otherwise assume $HOME/.config
+xdg-config-home = ''
+if 'XDG_CONFIG_HOME' in os.environ:
+    xdg-config-home = os.environ['XDG_CONFIG_HOME']
+else:
+    xdg-config-home = "%s/.config" % os.path.expanduser('~')
+
+# XDG base-dir: "If, when attempting to write a file, the destination directory is non-existant an attempt should be made to create it with permission 0700. If the destination directory exists already the permissions should not be changed."
+if not os.path.isdir(xdg-config-home):
+    os.mkdir(xdg-config-home, 0700)
+
+# finally create our own config dir
+config-dir = "%s/%s" % (xdg-config-home, 'python-cozify')
+if not os.path.isdir(config-dir):
+    os.mkdir(config-dir, 0700)
+
+stateFile = "%s/python-cozify.cfg" % config-dir
 state = None
 
 
