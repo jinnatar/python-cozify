74b9c3635320635fcc6ac048e969d2e6b817f01a
Artanicus <artanicus@nocturnal.fi>
2017-05-07 15:18:11 +0300

    Cheapen ping() and actually call it internally in getDevices()
    This may not be the best way to do this but it's a quick'n'dirty

--- cozify/hub.py
+++ cozify/hub.py
@@ -1,4 +1,4 @@
-import requests, json
+import requests, json, logging
 from . import config as c
 from . import cloud
 
@@ -11,8 +11,8 @@ def getDevices(hubName=None):
         hubName = getDefaultHub()
 
     configName = 'Hubs.' + hubName
-    if configName not in c.state or 'hubtoken' not in c.state[configName]:
-        print('Hub not known, auth needed first')
+    if configName not in c.state or 'hubtoken' not in c.state[configName] or not ping():
+        logging.warning('No valid authentication token, requesting authentication')
         cloud.authenticate()
 
     # at this stage we have a valid name and auth
@@ -42,11 +42,16 @@ def _getBase(host, port=8893, api=apiPath):
     # TODO(artanicus): this may still need some auth hook
     return 'http://%s:%s%s' % (host, port, api)
 
-# perform a small API call to trigger any potential APIError and return boolean for success/failure
-# TODO(artanicus): make the call actually small
-def ping():
+# perform a cheap API call to trigger any potential APIError and return boolean for success/failure
+def ping(hub_name=None):
+    global pings
+    if hub_name is None:
+        hub_name =  getDefaultHub()
     try:
-        getDevices()
+        config_name = 'Hubs.' + hub_name
+        hub_token = c.state[config_name]['hubtoken']
+        hub_host = c.state[config_name]['host']
+        tz = _tz(hub_host, hub_token)
     except APIError as e:
         if e.status_code == 401:
             return False
@@ -64,5 +69,17 @@ def _hub(host):
     response = requests.get(_getBase(host=host, api='/') + 'hub')
     if response.status_code == 200:
         return json.loads(response.text)
+    else:
+        raise APIError(response.status_code, response.text)
+
+# 1:1 implementation of /hub/tz API call
+# hubHost: valid ip/host to hub, defaults to state data
+# hub_token: authentication token
+# returns timezone of hub, e.g. Europe/Helsinki
+def _tz(host, hub_token):
+    headers = { 'Authorization': hub_token }
+    response = requests.get(_getBase(host=host) + 'hub/tz', headers=headers)
+    if response.status_code == 200:
+        return response.text
     else:
         raise APIError(response.status_code, response.text)
