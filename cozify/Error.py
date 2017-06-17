de00344c90abb3a070580f5d7935f8cc27cbc555
Artanicus <artanicus@nocturnal.fi>
2017-06-17 23:17:58 +0300

    Add basic test docstrings

--- cozify/Error.py
+++ cozify/Error.py
@@ -1,14 +1,18 @@
 class APIError(Exception):
+    """Error raised for non-200 API return codes"""
     def __init__(self, status_code, message):
         self.status_code = status_code
         self.message = message
 
     def __str__(self):
+        """Return error string with API status code and possible error message."""
         return 'API error, %s: %s' % (self.status_code, self.message)
 
 class AuthenticationError(Exception):
+    """Error raised for nonrecoverable authentication failures."""
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
+        """Return error string with possible error message."""
         return 'Authentication error: %s' % self.message
