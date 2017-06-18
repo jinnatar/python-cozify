dd6d0af5b162264f9e2583e622a7c9ef8c6d72e2
Artanicus <artanicus@nocturnal.fi>
2017-06-18 15:04:36 +0300

    Use RTD theme for local docs devel

--- docs/conf.py
+++ docs/conf.py
@@ -87,6 +87,9 @@ todo_include_todos = False
 #
 #html_theme = 'alabaster'
 
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
