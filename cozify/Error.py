c44eb5ecf3f0da8147cdc2488f502f81145264bc
Artanicus <artanicus@nocturnal.fi>
2017-06-18 14:11:22 +0300

    Improved exception documentation

--- cozify/Error.py
+++ cozify/Error.py
@@ -1,18 +1,34 @@
 class APIError(Exception):
-    """Error raised for non-200 API return codes"""
+    """Error raised for non-200 API return codes
+
+    Args:
+        status_code(int): HTTP status code returned by the API
+        message(str): Potential error message returned by the API
+    
+    Attributes:
+        status_code(int): HTTP status code returned by the API
+        message(str): Potential error message returned by the API
+    """
+
     def __init__(self, status_code, message):
         self.status_code = status_code
         self.message = message
 
     def __str__(self):
-        """Return error string with API status code and possible error message."""
         return 'API error, %s: %s' % (self.status_code, self.message)
 
 class AuthenticationError(Exception):
-    """Error raised for nonrecoverable authentication failures."""
+    """Error raised for nonrecoverable authentication failures.
+
+    Args:
+        message(str): Human readable error description
+    
+    Attributes:
+        message(str): Human readable error description
+    """
+
     def __init__(self, message):
         self.message = message
 
     def __str__(self):
-        """Return error string with possible error message."""
         return 'Authentication error: %s' % self.message
