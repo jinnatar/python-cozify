8e7ebb336e84d5c88fd3b45c2b2000ceefca6f23
Artanicus <artanicus@nocturnal.fi>
2017-10-15 17:00:01 +0300

    Plonk tests into setup.py, move version handling to module

--- setup.py
+++ setup.py
@@ -1,11 +1,27 @@
 from distutils.core import setup
+from setuptools.command.test import test as TestCommand
 
+import sys
+import cozify
 
 with open('README.rst') as file:
         long_description = file.read()
 
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
+
+
+
 setup(name='cozify',
-        version = '0.2.8',
+        version = cozify.__version__,
         author = 'artanicus',
         author_email = 'python-cozify@nocturnal.fi',
         url = 'https://github.com/Artanicus/python-cozify',
@@ -13,6 +29,9 @@ setup(name='cozify',
         long_description = long_description,
         license = 'MIT',
         packages = ['cozify'],
+	tests_require=['pytest'],
+	install_requires=['requests'],
+        cmdclass={'test': PyTest},
         classifiers = [
             "Development Status :: 3 - Alpha",
             "Topic :: Utilities",
