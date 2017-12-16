5f44c43781b4df40e8b60b8d7de02d9bb1304a17
Artanicus <artanicus@nocturnal.fi>
2017-12-16 20:52:09 +0200

    Simple JWT decoder for state file tokens

---
+++ util/tokenExplorer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import sys, pprint
+import jwt
+
+from cozify import hub, cloud, config
+
+def main(statepath):
+    config.setStatePath(statepath)
+
+    cloud_token = cloud.token()
+    hub_id = hub.getDefaultHub()
+    hub_token = hub.token(hub_id)
+
+    pp = pprint.PrettyPrinter(indent=2)
+    for token in cloud_token, hub_token:
+        claims = jwt.decode(token, verify=False)
+        pp.pprint(claims)
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        sys.exit(1)
