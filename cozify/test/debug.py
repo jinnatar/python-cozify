97db108b80e805ca110f974e9e498e1902a9d57e
Artanicus <artanicus@nocturnal.fi>
2017-12-18 15:43:56 +0200

    Clean debug down to essentials

--- cozify/test/debug.py
+++ cozify/test/debug.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python3
-"""Set high log level and consistent temporary state storage in /tmp/python-cozify-testing.cfg
+"""Set high log level
 """
 
 import logging
-from cozify import config
 
 logging.basicConfig(level=logging.DEBUG)
-
-# Disabled due to not wanting to mock the entire auth, so instead we use whatever is the live state.
-#conf_file='/tmp/python-cozify-testing.cfg'
-#config.setStatePath(conf_file)
