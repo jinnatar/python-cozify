f533bca40c4e17eb5623db47b18b816a54fa83f0
Artanicus <artanicus@nocturnal.fi>
2018-02-23 17:13:38 +0200

    Improved version of capabilities_list util, now things stay sorted

--- util/capabilities_list.py
+++ util/capabilities_list.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3                                                                                                                                         
 from cozify import hub
-from collections import OrderedDict
+import cozify
 
-def sort(tosort):
-    return list(OrderedDict.fromkeys(tosort))
-
+def dedup(seq):
+    seen = set()
+    seen_add = seen.add
+    return [x for x in seq if not (x in seen or seen_add(x))]
 
 def main():
     capabilities = []
-    devs = hub.getDevices()
+    devs = hub.devices()
     for id, dev in devs.items():
         capabilities = capabilities + dev['capabilities']['values']
 
-    gathered = sort(capabilities)
-    implemented = sort([ e.name for e in hub.capability ])
-    not_implemented = [item for item in gathered if item not in implemented]
-    composite = sort(implemented + not_implemented)
+    gathered = sorted(dedup(capabilities))
+    implemented = [ e.name for e in hub.capability ]
+    not_implemented = [ item for item in gathered if item not in implemented ]
+    composite = sorted(implemented + not_implemented)
 
+    print('Capabilities in python-cozify version {0}'.format(cozify.__version__))
     print('implemented ({0}): {1}'.format(len(implemented), implemented))
     print('gathered ({0}): {1}'.format(len(gathered), gathered))
     print('Not currently implemented ({0}): {1}'.format(len(not_implemented), not_implemented))
-    print('Fully updated capabilities string: {0}'.format(' '.join(composite)))
+    print('Fully updated capabilities string({0}): {1}'.format(len(composite), ' '.join(composite)))
 
 if __name__ == "__main__":
         main()
