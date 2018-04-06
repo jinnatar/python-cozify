314d45476bd079fe3b9bb97af181b2b6f306fe73
Artanicus <artanicus@nocturnal.fi>
2018-04-06 21:25:15 +0300

    Test for XDG basedir creation

--- cozify/test/test_config.py
+++ cozify/test/test_config.py
@@ -16,3 +16,12 @@ def test_config_XDG_env(tmp_hub):
         os.environ["XDG_CONFIG_HOME"] = td
         config.setStatePath(config._initXDG())
         assert td in config.state_file
+
+
+def test_config_XDG_basedir(tmp_hub):
+    # using mktemp deliberately to let _initXDG create it
+    td = tempfile.mktemp()
+    os.environ["XDG_CONFIG_HOME"] = td
+    assert config._initXDG()
+    assert os.path.isdir(td)
+    os.removedirs(td + '/python-cozify')
