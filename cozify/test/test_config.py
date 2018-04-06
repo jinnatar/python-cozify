1079ffccd0c9d5f390ecf35e7d4402e07bd99986
Artanicus <artanicus@nocturnal.fi>
2018-04-06 16:48:46 +0300

    More thorough testing of XDG setup. the _env test still fails .. unfortunately it fails for good reason as far as I can see.

--- cozify/test/test_config.py
+++ cozify/test/test_config.py
@@ -1,8 +1,21 @@
 #!/usr/bin/env python3
 
+import os, tempfile
+
 from cozify import config
 from cozify.test import debug
+from cozify.test.fixtures import tmp_hub
 
 
-def test_config_XDG():
-    print(config._initXDG())
+def test_config_XDG(tmp_hub):
+    assert config._initXDG()
+    config.dump_state()
+
+
+def test_config_XDG_env():
+    with tempfile.TemporaryDirectory() as td:
+        os.environ["XDG_CONFIG_HOME"] = td
+        print('Overriden config: {0}, config.state_file: {1}'.format(td, config.state_file))
+        assert config._initXDG()
+        assert td in config.state_file
+        config.dump_state()
