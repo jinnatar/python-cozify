d4e6de41ea8f5dc0887c1d4ac0459357d49a838c
Artanicus <artanicus@nocturnal.fi>
2018-04-07 22:01:35 +0300

    Add coloring to the fader to easier visualize errors

--- util/device-fade-test.py
+++ util/device-fade-test.py
@@ -10,9 +10,15 @@ flags.DEFINE_float('delay', 0.5, 'Step length in seconds.')
 flags.DEFINE_float('steps', 20, 'Amount of steps to divide into.')
 flags.DEFINE_bool('verify', False, 'Verify if value went through as-is.')
 
+green = '\u001b[32m'
+yellow = '\u001b[33m'
+red = '\u001b[31m'
+reset = '\u001b[0m'
+
 
 def main(argv):
     del argv
+    previous = None
     for step in numpy.flipud(numpy.linspace(0.0, 1.0, num=FLAGS.steps)):
         hub.light_brightness(FLAGS.device, step)
         time.sleep(FLAGS.delay)
@@ -23,9 +29,15 @@ def main(argv):
             read = devs[FLAGS.device]['state']['brightness']
             if step == read:
                 result = '✔'
+                color = green
             else:
                 result = '✖'
-        print('[{2}] set: {0} vs. read: {1}'.format(step, read, result))
+                if read == previous:
+                    color = yellow
+                else:
+                    color = red
+            previous = step
+        print('{3}[{2}] set: {0} vs. read: {1}{4}'.format(step, read, result, color, reset))
 
 
 if __name__ == "__main__":
