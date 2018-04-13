096abd069d9a2e662aae0de70d628efcd24ed66b
Artanicus <artanicus@nocturnal.fi>
2018-04-13 15:59:47 -0400

    Shut up urllib3 from test outputs

--- cozify/test/debug.py
+++ cozify/test/debug.py
@@ -3,5 +3,10 @@
 """
 
 from absl import logging
+import logging as dlog
+
+# shut up the hyperactive urllib3
+urllib3_logger = dlog.getLogger('urllib3')
+urllib3_logger.setLevel(dlog.CRITICAL)
 
 logging.set_verbosity(logging.DEBUG)
