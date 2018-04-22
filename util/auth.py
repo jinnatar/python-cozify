285435fd4db6108398aad82e4c49121509c6df11
Artanicus <artanicus@nocturnal.fi>
2018-04-22 17:15:19 +0300

    More versatile auth util

--- util/auth.py
+++ util/auth.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 from cozify import cloud
+from absl import flags, app
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_bool('debug', False, 'Enable debug output.')
 
 
-def main():
-    assert cloud.authenticate()
+def main(argv):
+    del argv
+    if FLAGS.debug:
+        from cozify.test import debug
+    cloud.authenticate()
 
 
 if __name__ == "__main__":
-    main()
+    app.run(main)
