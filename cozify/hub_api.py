e6d0925c27e081b5b16532f52158a19e493bf5d8
Artanicus <artanicus@nocturnal.fi>
2018-05-04 10:20:17 +0300

    Bump hub api version to match latest hub software version

--- cozify/hub_api.py
+++ cozify/hub_api.py
@@ -11,7 +11,7 @@ from cozify import cloud_api
 from .Error import APIError
 from requests.exceptions import RequestException
 
-apiPath = '/cc/1.8'
+apiPath = '/cc/1.9'
 
 
 def _getBase(host, port=8893):
