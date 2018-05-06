cd0d85e6a3ac5a26088875fd75f7ad4e3420f121
Artanicus <artanicus@nocturnal.fi>
2018-05-06 10:27:13 +0300

    New integration test to make sure hub.ping() will also autorenew cloud
    It's failing right now, which is a good thing because it means a
    scenario I thought might be broken is indeed broken but now I know and
    can fix it.

---
+++ cozify/test/test_integration.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import pytest
+
+from cozify import hub, cloud
+from cozify.test import debug
+from cozify.test.fixtures import *
+from cozify.Error import APIError
+
+
+@pytest.mark.destructive
+def test_integration_ping_autorenew(live_hub, live_cloud):
+    # make sure state is as expected before mangling it
+    assert live_hub.ping()
+    assert live_cloud.ping()
+
+    # trash the hub_token
+    hub_id = live_hub.default()
+    live_hub.token(hub_id=hub_id, new_token='destroyed-on-purpose-by-destructive-integration-test')
+    assert not live_hub.ping(autorefresh=False)
+    with pytest.raises(APIError):
+        hub.tz()
+    assert live_hub.ping(autorefresh=True)
+
+    ancient_date = '2000-01-01T00:00:00'
+    live_cloud._setAttr('last_refresh', ancient_date, commit=False)
+    assert live_hub.ping(autorefresh=True)
+    assert live_cloud._getAttr('last_refresh') != ancient_date, 'cloud was not autorefreshed'
