41f0aba9b214aac6652f9945840d706eb0a85402
Artanicus <artanicus@nocturnal.fi>
2018-10-21 15:55:05 +0300

    JWT token decoder

---
+++ util/tokendecode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import sys, pprint
+import jwt
+from datetime import datetime
+
+def main(token):
+    pp = pprint.PrettyPrinter(indent=2)
+    claims = jwt.decode(token, verify=False)
+    for stamp in ['exp', 'iat']:
+        ts = int(claims[stamp])
+        claims[stamp] = datetime.utcfromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+    
+    pp.pprint(claims)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        main(sys.argv[1])
+    else:
+        sys.exit(1)
