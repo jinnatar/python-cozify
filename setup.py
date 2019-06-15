cf06d0c3b77ec12fd0e4f7bc8bdfe690121c3b80
Artanicus <artanicus@nocturnal.fi>
2019-06-15 17:00:08 +0300

    Start testing against python-3.7

--- setup.py
+++ setup.py
@@ -29,4 +29,5 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7'
     ])  # yapf: disable
