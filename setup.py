b2f1c1c54f3ebb3c95de0032fc13fc03997d222c
Artanicus <artanicus@nocturnal.fi>
2021-02-18 23:15:19 +0200

    Fix python version syntax errors, bump minimum to 3.6 [no ci]

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.rst') as file:
 setup(
     name='cozify',
     version=cozify.__version__,
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     author='artanicus',
     author_email='python-cozify@nocturnal.fi',
     url='https://github.com/Artanicus/python-cozify',
@@ -27,8 +27,8 @@ setup(
         'Topic :: Utilities',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9'
     ])  # yapf: disable
